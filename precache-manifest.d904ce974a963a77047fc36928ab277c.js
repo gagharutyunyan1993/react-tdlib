27b58aea376bc5bd3786d9040cf2a02797ec4f11
Gagik Harutyunyan <gagharutyunyan1993@gmail.com>
2021-10-11 23:28:23 +0400

    Updates

---
+++ precache-manifest.d904ce974a963a77047fc36928ab277c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153192946ccf21bc4ca35dd56127ff1d",
+    "url": "/react-tdlib/index.html"
+  },
+  {
+    "revision": "ed7e3d7c931c2466247b",
+    "url": "/react-tdlib/static/css/0.9b2f7aac.chunk.css"
+  },
+  {
+    "revision": "ad0e38bb6a39291a5b3f",
+    "url": "/react-tdlib/static/css/3.3b62f73a.chunk.css"
+  },
+  {
+    "revision": "3e8e2b62d90785c9f42c",
+    "url": "/react-tdlib/static/css/5.790112a5.chunk.css"
+  },
+  {
+    "revision": "e9e0999eaa3b24066ae0",
+    "url": "/react-tdlib/static/css/6.1a62a153.chunk.css"
+  },
+  {
+    "revision": "3c9cc13e743710ddbb1c",
+    "url": "/react-tdlib/static/css/main.ee7fa5df.chunk.css"
+  },
+  {
+    "revision": "ed7e3d7c931c2466247b",
+    "url": "/react-tdlib/static/js/0.9e08c1bb.chunk.js"
+  },
+  {
+    "revision": "ad0e38bb6a39291a5b3f",
+    "url": "/react-tdlib/static/js/3.2b50e76b.chunk.js"
+  },
+  {
+    "revision": "f1aa306a9a39065c445d",
+    "url": "/react-tdlib/static/js/4.4bf8fb5a.chunk.js"
+  },
+  {
+    "revision": "945d79085d076fa3e227aeebec57b50b",
+    "url": "/react-tdlib/static/js/4.4bf8fb5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e8e2b62d90785c9f42c",
+    "url": "/react-tdlib/static/js/5.bb3d97e8.chunk.js"
+  },
+  {
+    "revision": "5092c6e04c7faa4b291d51dc5bb6081a",
+    "url": "/react-tdlib/static/js/5.bb3d97e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e0999eaa3b24066ae0",
+    "url": "/react-tdlib/static/js/6.275da7ec.chunk.js"
+  },
+  {
+    "revision": "d75118724e4af61f835b",
+    "url": "/react-tdlib/static/js/7.a1050862.chunk.js"
+  },
+  {
+    "revision": "3c9cc13e743710ddbb1c",
+    "url": "/react-tdlib/static/js/main.27bdb34b.chunk.js"
+  },
+  {
+    "revision": "989c7d55321ce3ea4fdc",
+    "url": "/react-tdlib/static/js/runtime-main.125198a3.js"
+  },
+  {
+    "revision": "c6a92d89aa71027ea621d52bb03ad681",
+    "url": "/react-tdlib/static/media/bubble-tail-left.c6a92d89.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/react-tdlib/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "fb38f7d6fbf79f207edfa158c10f16a7",
+    "url": "/react-tdlib/static/media/check.fb38f7d6.svg"
+  },
+  {
+    "revision": "8e3f975b0dac0d7ddc9f5ab85f24d15a",
+    "url": "/react-tdlib/static/media/telegram-logo.8e3f975b.svg"
+  }
+]);
